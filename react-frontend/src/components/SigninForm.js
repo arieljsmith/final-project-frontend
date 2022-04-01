6759078ffa350aff44253c0cbf782e0e1130534c
UKWN8 <UKWN808@gmail.com>
2022-04-01 16:55:55 -0400

    Removed navbar and is now ready for merge

--- react-frontend/src/components/SigninForm.js
+++ react-frontend/src/components/SigninForm.js
@@ -21,58 +21,6 @@ function SigninForm() {
 
   return (
     <div>
-      {/* BEGIN HERO SECTION */}
-      <header>
-        <nav className="bg-white shadow dark:bg-gray-800">
-          <div className="container px-6 py-4 mx-auto">
-            <div className="lg:flex lg:items-center lg:justify-between">
-              <div className="flex items-center justify-between">
-                <div className="text-xl font-semibold text-gray-700">
-                  <a
-                    className="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300"
-                    href="#"
-                  >
-                    Restaurant Recommendations
-                  </a>
-                </div>
-
-                {/* Mobile menu button */}
-                <div className="flex lg:hidden">
-                  <button
-                    type="button"
-                    className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
-                    aria-label="toggle menu"
-                  >
-                    <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
-                      <path
-                        fill-rule="evenodd"
-                        d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                      ></path>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-
-              {/* Mobile Menu open: "block", Menu closed: "hidden" */}
-              <div className="hidden -mx-4 lg:flex lg:items-center">
-                <a
-                  href="/sign-in"
-                  className="block mx-4 mt-2 text-sm text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400"
-                >
-                  Sign In
-                </a>
-                <a
-                  href="/sign-up"
-                  className="block mx-4 mt-2 text-sm text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400"
-                >
-                  Sign Up
-                </a>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </header>
-      {/* END HERO SECTION */}
       <div className="grid place-items-center  p-8  h-screen form-container">
         <div className="grid place-items-center">
           <h1 className="text-4xl">Sign In</h1>
