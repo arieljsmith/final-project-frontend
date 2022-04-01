aeb0ba87d2bf97c057bf79abb8154c64979d9184
Ariel Smith <arieljeansmith@gmail.com>
2022-04-01 13:46:38 -0700

    Misc Details

--- react-frontend/src/components/HomeHero.js
+++ react-frontend/src/components/HomeHero.js
@@ -1,18 +1,19 @@
 function HomeHero() {
     return (
-        <header>
+        <header className="mb-16">
         <div className="w-full bg-center bg-cover h-[32rem]" style={{backgroundImage: "url('https://i.imgur.com/WbsNcqD.png')"}}>
             <div className="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50">
                 <div className="text-center">
                     <h1 className="text-2xl font-semibold text-white uppercase lg:text-3xl">Restaurant Recommendations</h1>
-                    <p className="mt-3 text-gray-100 m-16">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna duis convallis convallis tellus id interdum. Velit dignissim sodales ut eu sem.</p>
+                    <p className="mt-3 text-gray-100 m-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna duis convallis convallis tellus id interdum. Velit dignissim sodales ut eu sem. Amet, consectetur adipiscing elit, sed do eiusmod.</p>
                     {/* SEARCH BAR START */}
                     <div className="pt-2 relative mx-auto text-gray-600">
-                        <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-4 rounded-lg text-sm focus:outline-none" type="search" name="search" placeholder="Search" />
+                        <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-4 rounded-lg text-sm focus:outline-none inline" type="search" name="search" placeholder="Search" />
+                        <button className="w-auto px-4 py-2 mt-4 text-sm ml-4 font-medium text-white uppercase transition-colors duration-200 transform bg-rose-600 rounded-full lg:w-auto hover:bg-rose-500 focus:outline-none focus:bg-rose-500 inline">+</button>
                     </div>
                     {/* SEARCH BAR END */}
                     {/* ADD REC BUTTON START */}
-                    <button className="w-auto px-4 py-2 mt-4 text-sm font-medium text-white uppercase transition-colors duration-200 transform bg-rose-600 rounded-md lg:w-auto hover:bg-rose-500 focus:outline-none focus:bg-rose-500">+</button>
+                    
                     {/* ADD REC BUTTON END */}
                 </div>
             </div>
--- react-frontend/src/pages/Homepage.js
+++ react-frontend/src/pages/Homepage.js
@@ -8,8 +8,19 @@ function Homepage() {
         <div className="bg-slate-100">
             <NavBar />
             <HomeHero />
-            <h2 className="text-2xl font-semibold text-black uppercase lg:text-3xl mt-20 mr-4 mb-8 ml-6">Friend Recommendations</h2>
-            <div className="grid grid-cols-2 md:grid-cols-4 m-4">
+            <div className="mb-6">
+                <h2 className="text-2xl font-semibold text-black uppercase lg:text-3xl mt-20 mr-4 mb-8 ml-6 inline">Recommendations</h2>
+                <button className="inline w-auto px-4 py-2 mt-4 text-sm font-medium text-white uppercase transition-colors duration-200 transform bg-rose-600 rounded-full lg:w-auto hover:bg-rose-500 focus:outline-none focus:bg-rose-500">+</button>
+            </div>
+            <div className="grid grid-cols-2 md:grid-cols-4 mt-4 mr-4 ml-4 mb-14">
+                <HomeFriendRecommendation />
+                <HomeFriendRecommendation />
+                <HomeFriendRecommendation />
+                <HomeFriendRecommendation />
+                <HomeFriendRecommendation />
+                <HomeFriendRecommendation />
+                <HomeFriendRecommendation />
+                <HomeFriendRecommendation />
                 <HomeFriendRecommendation />
                 <HomeFriendRecommendation />
                 <HomeFriendRecommendation />
