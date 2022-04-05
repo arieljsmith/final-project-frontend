2028a34c5282a29e34fd1b536c74c08000ab27b3
Ariel Smith <arieljeansmith@gmail.com>
2022-04-05 14:16:51 -0700

    Create LocationPage.js; populate with some code; connect it to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import SignupForm from "./pages/SignupForm";
 import SigninForm from "./pages/SigninForm";
 import Homepage from './pages/Homepage';
+import LocationPage from './pages/LocationPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/sign-up" element={<SignupForm />} />
       <Route path="/sign-in" element={<SigninForm />} />
       <Route path="/home" element={<Homepage />} />
+      <Route path="/locationskelly" element={<LocationPage />} />
     </Routes>
   );
 }
---
+++ src/pages/LocationPage.js
@@ -0,0 +1,13 @@
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+
+function LocationPage() {
+    return (
+        <div>
+            <p>This is a page. For a location. What more do you want?</p>
+            <p>(Ok, lots more, but bear with me.)</p>
+        </div>
+    );
+}
+
+export default LocationPage;
