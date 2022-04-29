afb39bcd83c7f1221f6f05d509eea467c03fff2e
Ariel Smith <arieljeansmith@gmail.com>
2022-04-28 22:03:24 -0700

    Tidy up Sign In and Sign Up Pages

--- src/pages/SigninForm.js
+++ src/pages/SigninForm.js
@@ -43,20 +43,20 @@ export default function SigninForm() {
   return (
     
     <>
-    <NavBar />
-      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    {/* <NavBar /> */}
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 recco-roboto-text">
       
         <div className="max-w-md w-full space-y-8">
           <div>
             <img
               className="mx-auto h-12 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              src="../../recologo.png"
               alt="Workflow"
             />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               Or{' '}
-              <a href="/signup" className="font-medium text-indigo-600 hover:text-indigo-500">
+              <a href="/signup" className="font-medium text-red-600 hover:text-red-500">
                 Sign Up for a new account
               </a>
             </p>
@@ -98,7 +98,7 @@ export default function SigninForm() {
               </div>
             </div>
 
-            <div className="flex items-center justify-between">
+            {/* <div className="flex items-center justify-between">
               <div className="flex items-center">
                 <input
                   id="remember-me"
@@ -116,15 +116,15 @@ export default function SigninForm() {
                   Forgot your password?
                 </a>
               </div>
-            </div>
+            </div> */}
 
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                  <LockClosedIcon className="h-5 w-5 text-white group-hover:text-white" aria-hidden="true" />
                 </span>
                 Sign in
               </button>
--- src/pages/SignupForm.js
+++ src/pages/SignupForm.js
@@ -47,20 +47,20 @@ export default function SignunForm() {
   return (
     
     <>
-    <NavBar />
-      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    {/* <NavBar /> */}
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 recco-roboto-text">
       
         <div className="max-w-md w-full space-y-8">
           <div>
             <img
               className="mx-auto h-12 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              src="../recologo.png"
               alt="Workflow"
             />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign up for a New Account</h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               Or if you already have an account,{' '}
-              <a href="/signin" className="font-medium text-indigo-600 hover:text-indigo-500">
+              <a href="/signin" className="font-medium text-red-600 hover:text-red-500">
                 Sign In
               </a>
             </p>
@@ -102,7 +102,7 @@ export default function SignunForm() {
                   placeholder="Email address"
                 />
               </div>
-              <p className="mt-2 text-base text-gray-600">Password</p>
+              <p className="text-base text-gray-600">Password</p>
               <div>
                 <label htmlFor="password" className="sr-only">
                   Password
@@ -126,7 +126,7 @@ export default function SignunForm() {
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 Sign Up
               </button>
