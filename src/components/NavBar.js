e9dbb87bc7f97a7a784dce8b8f61baaf0a3a2131
Ariel Smith <arieljeansmith@gmail.com>
2022-04-28 21:12:25 -0700

    Navbar dropshadow added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -30,7 +30,7 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <Disclosure as="nav" className="bg-red-600 recco-roboto-text">
+    <Disclosure as="nav" className="bg-red-600 recco-roboto-text drop-shadow-xl">
       {({ open }) => (
         <>
           <div className="max-w-9xl mx-auto px-2 sm:px-6 lg:px-8">
